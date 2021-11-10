7483e28890a3a55431994d83f0cea97a7fdb8226
teerawat prarom <teewat@yahoo.com>
2021-11-10 12:11:29 +1100

    completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :word
+    def initialize(word)
+        @word = word
+    end
+
+    def match(arr_words)
+        res_arr = []
+        arr_words.each do |word|
+           if word.split("").sort == @word.split("").sort
+                res_arr << word
+           end
+        end
+        res_arr
+        
+    end
+end
